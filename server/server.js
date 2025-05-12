495f00d7f4e467bc0f67d7266bdd783c9bf0d780
Patil, Kiran (SI EA R&D IN KNE SW) <patil.kiran@siemens.com>
2025-05-12 15:44:58 +0530

    home page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Serve static files (HTML, CSS, JS) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const planRoute = require("./routes/plan");
 app.use("/api", planRoute);
