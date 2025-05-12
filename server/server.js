2975b1e262344cf8f4d561dfaee6d8a30a8cf2b1
Patil, Kiran (SI EA R&D IN KNE SW) <patil.kiran@siemens.com>
2025-05-12 16:50:42 +0530

    html

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config(); // To load environment variables like API keys
-const path = require("path");
+require("dotenv").config(); // To load environment variables like API keys
+const path = require("path"); // To handle file paths
 
 const app = express();
 app.use(cors()); // Allow cross-origin requests
 app.use(express.json()); // Parse JSON bodies from requests
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "client","public")));
-
-// Add a route for the root URL to serve the index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client","public", "index.html"));
-});
+// Serve static files (HTML, CSS, JS) from the 'client/public' folder
+app.use(express.static(path.join(__dirname, "client", "public")));
 
 // Import the routes
 const planRoute = require("./routes/plan");
 app.use("/api", planRoute); // Use /api prefix for all routes in the planRoute file
+
+// Catch-all route to serve the index.html for all non-API requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
